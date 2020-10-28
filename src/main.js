30efffe6fb690af26dbdd493084f13bb5e972a84
thegamerx1 <34986564+thegamerx1@users.noreply.github.com>
2020-10-28 21:14:09 +0100

    add blind

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 function random(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min }
+function triggerMouseEvent(node, eventType) {var clickEvent = document.createEvent('MouseEvents');clickEvent.initEvent(eventType, true, true);node.dispatchEvent(clickEvent);}
 hackclass = class {
 	constructor() {
 		this.anagram = document.getElementById("currentWord")
@@ -12,27 +13,47 @@ hackclass = class {
 
 	getdata() {
 		var data = {}
+		data.game = document.getElementById("screenGame")
 		data.time = document.getElementById("timer").innerHTML
 		var players = document.getElementById("containerGamePlayers").childNodes
 		players.forEach(player => {
 			if (player.querySelector(".name").style.color == "rgb(0, 0, 255)") {
 				data.player = player
 				data.guessed = player.classList.contains("guessedWord")
+				data.drawing = (player.querySelector(".avatar .drawing").style.display !== "none")
 			}
 		});
+		data.canvas = document.getElementById("canvasGame")
 		return data
 	}
 
 	step() {
 		this.loopcount++
 		var data = this.getdata()
-		if (data.guessed || data.time <= 1) {
-			this.type("https://git(dot)io/JT1KB")
-			this.continue()
+		if (data.drawing) {
+			if (!this.wasdrawing) {
+				data.game.querySelectorAll("div.containerToolbar div.containerTools div.tool img.toolIcon")[2].click()
+			}
+			if (this.loopcount % 2 == 0) {
+				document.getElementById("buttonClearCanvas").click()
+				data.game.querySelectorAll("div.containerToolbar div.containerTools div.tool img.toolIcon")[2].click()
+				var colors = data.game.querySelector("div.containerColorbox div.containerColorColumn").childNodes
+				colors[random(0, colors.length - 1)].click()
+			} else {
+				triggerMouseEvent(data.canvas, "mouseover")
+				triggerMouseEvent(data.canvas, "mousedown")
+				triggerMouseEvent(data.canvas, "mouseup")
+				triggerMouseEvent(data.canvas, "click")
+			}
+		}
+		if (data.guessed || data.time <= 1 || data.drawing) {
+			if (this.loopcount % 20 == 0) this.type("https://git(dot)io/JT1KB")
+			this.continue(100)
 			return
 		}
 		if (this.oldtime < data.time) {
 			this.sent = []
+			this.wasdrawing = false
 			this.wait = 0
 			console.debug("New")
 		}
@@ -63,17 +84,16 @@ hackclass = class {
 			this.type(word)
 			this.sent.push(word)
 			this.oldword = word
-			if (this.getdata().guessed) break
 		}
 		if (loops == 3) this.type("https://git(dot)io/JT1KB")
 		this.continue()
 	}
 
-	continue() {
+	continue(time) {
 		if (!this.running) return
 		let timenow = performance.now()
 		this.wait = timenow + random(2000,2500)
-		let timetowait = this.wait - timenow
+		let timetowait = (time) ? time : this.wait - timenow
 		setTimeout(this.step.bind(this), timetowait)
 	}
 
